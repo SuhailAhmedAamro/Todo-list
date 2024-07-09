d44abda3a7fe0db72c9fdd4918b0b0f32ee2294f
Suhai Ahmed <155607261+SuhailAhmedAamro@users.noreply.github.com>
2024-07-09 17:42:42 +0500

    Add files via upload

---
+++ TODO-LIST.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer"
+let todos= [];
+let condition = true;
+while(condition)
+{
+let addTask = await inquirer.prompt([{
+name : "todo",
+type : "input",
+message : "Eid ki Shoping ki hi"
+},
+{
+name: "Addmore",
+type: "confirm",
+message: "Aur kya khareeda",
+default : "false"
+}
+
+]);
+todos.push(addTask.todo)
+condition = addTask.Addmore
+console.log(todos);
+
+
+
+
+
+
+
+}
